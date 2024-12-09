805268809f7d4f03e1fde71c68505ca96e6ebd92
Myriam <myriiamwayne@gmail.com>
2024-12-09 15:58:03 +0100

    using in-order traversal

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - Entry point
+ *@tree: tree
+ *@func: func
+ * Return: Always 0 (Success)
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);/*The value in the node*/
+	binary_tree_inorder(tree->right, func);
+}
