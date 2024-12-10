5b8b20085fb793eaa87b362a50caa6877c40a9f3
Myriam <myriiamwayne@gmail.com>
2024-12-10 15:23:38 +0100

    Enregistrement des modifications avant le changement de branche

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,13 +2,15 @@
 #include "binary_trees.h"
 #include <stdio.h>
 /**
- * main - Entry point
- *
+ * binary_tree_node - Entry point
+ *@parent: parent
+ *@value: value
  * Return: Always 0 (Success)
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newNode = (binary_tree_t*)malloc(sizeof(binary_tree_t));
+	binary_tree_t *newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+
 	if (newNode != NULL)
 	{
 	newNode->parent = parent;
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "binary_trees.h"
 
 /**
@@ -9,17 +10,26 @@
 int main(void)
 {
 	binary_tree_t *root;
+	binary_tree_t *sibling;
 
 	root = binary_tree_node(NULL, 98);
-
 	root->left = binary_tree_node(root, 12);
-	root->left->left = binary_tree_node(root->left, 6);
-	root->left->right = binary_tree_node(root->left, 16);
-
-	root->right = binary_tree_node(root, 402);
-	root->right->left = binary_tree_node(root->right, 256);
-	root->right->right = binary_tree_node(root->right, 512);
+	root->right = binary_tree_node(root, 128);
+	root->left->right = binary_tree_node(root->left, 54);
+	root->right->right = binary_tree_node(root->right, 402);
+	root->left->left = binary_tree_node(root->left, 10);
+	root->right->left = binary_tree_node(root->right, 110);
+	root->right->right->left = binary_tree_node(root->right->right, 200);
+	root->right->right->right = binary_tree_node(root->right->right, 512);
 
 	binary_tree_print(root);
+	sibling = binary_tree_sibling(root->left);
+	printf("Sibling of %d: %d\n", root->left->n, sibling->n);
+	sibling = binary_tree_sibling(root->right->left);
+	printf("Sibling of %d: %d\n", root->right->left->n, sibling->n);
+	sibling = binary_tree_sibling(root->left->right);
+	printf("Sibling of %d: %d\n", root->left->right->n, sibling->n);
+	sibling = binary_tree_sibling(root);
+	printf("Sibling of %d: %p\n", root->n, (void *)sibling);
 	return (0);
 }
