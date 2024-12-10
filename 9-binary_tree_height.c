b65685976de8de6bfba4360433c3006194408e66
Myriam <myriiamwayne@gmail.com>
2024-12-10 17:13:33 +0100

    binary tree height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,20 +9,20 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left = 0;
+	size_t right = 0;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	if (left_height > right_height)
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (!tree->left && !tree->right)
+		return (0);
+	if (left > right)
 	{
-		return (left_height + 1);
+		return (left + 1);
 	}
-	else
-	{
-		return (right_height + 1);
-	}
+	return (right + 1);
 }
